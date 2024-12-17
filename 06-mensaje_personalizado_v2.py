af872c7f366eab3ce1371ae766cecd7aef96b98d
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-17 11:12:20 -0500

    Mensaje Personalizado Versión 2

---
+++ 06-mensaje_personalizado_v2.py
@@ -0,0 +1,13 @@
+"""
+Programa que muestra un mensaje personalizado en una sola línea
+Autor: Julian Esteban Gutiérrez
+Estudiante: Sharif Giraldo Obando
+Fecha: Diciembre de 2024
+Licencia GNU GPL v3
+Rec
+ursos de información: https://www.youtube.com/@infraestructura-linux-programa
+"""
+
+
+
+print("Bienvenid@"+input("Ingrese el nombre del cliente")+"a la tienda")
