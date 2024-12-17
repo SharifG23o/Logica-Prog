97626d3b5729fe3d438ea46527e25e09473f8591
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-17 11:03:43 -0500

    Módulo de apoyo

---
+++ apoyo.py
@@ -0,0 +1,26 @@
+"""
+Módulo de apoyo con funciones  
+Autor: Julian Esteban Gutiérrez
+Estudiante: Sharif Giraldo Obando
+Fecha: Diciembre de 2024
+Licencia GNU GPL v3
+Recursos de información: https://www.youtube.com/@infraestructura-linux-programa
+"""
+
+# MOSTRAR MENSAJE
+
+
+def mostrar_mensaje(mensaje):  # Aquí mensaje es recibido como parámetro
+    """
+    Función para mostrar un mensaje en pantalla
+    Parámetros:
+    -Mensaje a mostrar
+    """
+    print(mensaje)  # El parámetro es impreso
+
+
+# INGRESAR TEXTO
+
+def ingresar_texto(mensaje):
+    texto = input(mensaje)
+    return texto
