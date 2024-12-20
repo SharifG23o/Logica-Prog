6b34a34c7c1bca2eab763c5ecd3607934e840f7a
SharifG23o <sharifgiraldo23@gmail.com>
2024-12-20 11:50:46 -0500

    Evaluando expresiones

---
+++ 11-evaluando_expresiones.py
@@ -0,0 +1,12 @@
+"""
+Ejemplos de evaluar expresiones en Python
+Autor: Julian Esteban Gutiérrez
+Estudiante: Sharif Giraldo Obando
+Fecha: Diciembre de 2024
+Licencia GNU GPL v3
+Recursos de información: https://www.youtube.com/@infraestructura-linux-programa
+"""
+
+
+expresion1 = (3+2)*4 < 3+6*2 or 7//2 <= 2**(3+1) and 4 == 7 % 2
+print(expresion1)
