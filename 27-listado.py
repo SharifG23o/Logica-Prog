5b6bc669a5b4ac09bd3daf39db65d086e9842561
SharifG23o <sharifgiraldo23@gmail.com>
2025-01-04 20:02:46 -0500

    Listado de asistencia

---
+++ 27-listado.py
@@ -0,0 +1,33 @@
+"""
+Programa que genera un listado de asistencia
+Autor: Julián Esteban Gutiérrez
+Estudiante: Sharif Giraldo Obando
+Fecha: Enero de 2025
+Licencia GNU GPL v3
+Recursos de información: https://www.youtube.com/@infraestructura-linux-programa
+"""
+
+from apoyo import ingresar_entero_mayor_que, mostrar_mensaje
+
+VALOR_INICIAL = 0
+
+
+def main():
+    valor_inicial = ingresar_entero_mayor_que(
+        "Ingrese el valor inicial: ", VALOR_INICIAL)
+    valor_final = ingresar_entero_mayor_que(
+        "Ingrese el valor final: ", valor_inicial)
+    listado = generar_listado(valor_inicial, valor_final)
+    mostrar_mensaje(listado)
+
+
+def generar_listado(valor_inicial, valor_final):
+    listado = ""
+    i = valor_inicial
+    while i <= valor_final:
+        listado += f"{i:2d}.______________ \n"
+        i = i+1
+    return listado
+
+
+main()
