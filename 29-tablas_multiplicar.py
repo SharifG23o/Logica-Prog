46f26dd16eb1e5f553a7615e0b57fb642282100a
SharifG23o <sharifgiraldo23@gmail.com>
2025-01-09 22:20:26 -0500

    Tablas de multiplicar

---
+++ 29-tablas_multiplicar.py
@@ -0,0 +1,40 @@
+"""
+Programa que genera las tablas de multiplicar en un rango
+Autor: Julián Esteban Gutiérrez
+Estudiante: Sharif Giraldo Obando
+Fecha: Enero de 2025
+Licencia GNU GPL v3
+Recursos de información: https://www.youtube.com/@infraestructura-linux-programa
+"""
+
+from apoyo import ingresar_entero_rango, generar_mensaje_tabla_multiplicar, mostrar_mensaje
+
+MINIMO_VALOR_TABLA = 0
+MAXIMO_VALOR_TABLA = 10
+MINIMA_TABLA = 0
+MAXIMA_TABLA = 20
+
+
+def main():
+
+    limite_inferior = ingresar_entero_rango(
+        "Ingrese el límite inferior: ", MINIMA_TABLA, MAXIMA_TABLA)
+    limite_superior = ingresar_entero_rango(
+        "Ingrese el límite superior: ", limite_inferior, MAXIMA_TABLA)
+    mensaje_tablas = generar_tablas_multiplicar(
+        limite_inferior, limite_superior, MINIMO_VALOR_TABLA, MAXIMO_VALOR_TABLA)
+    mostrar_mensaje(mensaje_tablas)
+
+
+def generar_tablas_multiplicar(limite_inferior, limite_superior, valor_minimo, valor_maximo):
+    mensaje_tablas = f"TABLAS DE MULTIPLICACIÓN ENTRE {
+        limite_inferior} y {limite_superior} \n"
+    i = limite_inferior
+    while i <= limite_superior:
+        mensaje_tablas += generar_mensaje_tabla_multiplicar(
+            i, valor_minimo, valor_maximo)
+        i += 1
+    return mensaje_tablas
+
+
+main()
