3d22b6d7d9874e5e89c390c4fcef31d44ef7cee2
SharifG23o <sharifgiraldo23@gmail.com>
2025-01-10 07:21:15 -0500

    Contar_vocales

---
+++ 30-contar_vocales.py
@@ -0,0 +1,42 @@
+"""
+Programa que cuenta el número de vocales empleadas en una frase
+Autor: Julián Esteban Gutiérrez
+Estudiante: Sharif Giraldo Obando
+Fecha: Enero de 2025
+Licencia GNU GPL v3
+Recursos de información: https://www.youtube.com/@infraestructura-linux-programa
+"""
+
+
+from apoyo import ingresar_texto, verificar_es_vocal, mostrar_mensaje
+
+
+def main():
+
+    frase = ingresar_texto("Ingrese la frase o respuesta del encuestado: ")
+    cantidad_vocales = obtener_cantidad_vocales(frase)
+    mensaje_vocales = generar_mensaje_vocales(frase, cantidad_vocales)
+    mostrar_mensaje(mensaje_vocales)
+
+
+def obtener_cantidad_vocales(frase):
+
+    cantidad_vocales = 0
+
+    for letra in frase:
+
+        if verificar_es_vocal(letra):
+            cantidad_vocales = cantidad_vocales+1
+    return cantidad_vocales
+
+
+
+def generar_mensaje_vocales(frase, cantidad_vocales):
+
+    mensaje = f"La frase \"{frase}\" tiene {cantidad_vocales} vocales"
+    return mensaje
+
+
+
+
+main()
